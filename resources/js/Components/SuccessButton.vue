8a16f39cf1b71a74409f79b41193ad9fd1049318
Lutreze Hue Jacinto <86185117+nocturnalDev11@users.noreply.github.com>
2024-12-27 17:44:40 +0000

    commit

--- resources/js/Components/SuccessButton.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <button
-        class="inline-flex items-center rounded-md border border-transparent bg-green-600 px-4 py-2 text-xs font-semibold uppercase tracking-widest text-white transition duration-150 ease-in-out hover:bg-green-500 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 active:bg-green-700 dark:focus:ring-offset-gray-800">
-        <slot />
-    </button>
-</template>
